cad8d02c246f566e835da3c6778c28b0543009be
Mateusz Zajac <deejay.zajac@gmail.com>
2017-01-16 18:00:13 +0100

    Added DeepStream first implementation

---
+++ DeepStream/app.js
@@ -0,0 +1,23 @@
+const DeepstreamServer = require('deepstream.io')
+const C = DeepstreamServer.constants
+
+var Deepstream = require( 'deepstream.io' );
+/*
+var RedisMessageConnector = require( 'deepstream.io-msg-redis' );
+*/
+var server = new Deepstream({
+  host: '0.0.0.0',
+  port: 8080
+});
+/*
+server.set( 'messageConnector', new RedisMessageConnector( {
+  port: 6379,
+  host: 'wrredis-001.zqouos.0001.usw2.cache.amazonaws.com'
+}));*/
+
+// start the server
+server.start()
+
+
+
+
